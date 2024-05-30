93bbb4213b1f0237e446bcb8604896794a613694
Mathias-Kabango3 <m.kabango@alustudent.com>
2024-05-31 00:10:21 +0200

    add static files

--- workspace/Littlelemon/Littlelemon/settings.py
+++ workspace/Littlelemon/Littlelemon/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "Littlelemon.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR/'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -115,8 +115,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+import os
 
 STATIC_URL = "static/"
+STAICFILES_DIRS = (os.path.join(BASE_DIR/'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
